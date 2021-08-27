29ab318f2f4cc43839b9297a9585a8ca006f84bc
syoung7388 <79610047+syoung7388@users.noreply.github.com>
2021-08-28 07:27:56 +0900

    08_28

--- test.py
+++ test.py
@@ -1,39 +1,27 @@
-import sys
 
-n, m = map(int, sys.stdin.readline().split())
+def getRes(x, y, dy):
+    r = 2147000000
+    print("=========")
+    for i in range(x+1, y):
+        print(i, end=" ")
+        if dy[i-1]&dy[i] == 0:   
+            return 0
+        else:
+            r = min(dy[i-1]&dy[i],r)
+    print(r)
+    return r
+                
 
-data = [(0, 0)]
-light = [0]
-for _ in range(n):
-    a, b = map(int, sys.stdin.readline().split())
-    data.append((a, b))
-    light.append(light[-1] + b)
-
-dp = [[[float('inf'), float('inf')] for _ in range(n + 1)] for _ in range(n + 1)]
-dp[1][n][0] = dp[1][n][1] = 0
-
-
-def f(i, j, flag):
-    if dp[i][j][flag] < float('inf'):
-        return dp[i][j][flag]
-
-    if flag:
-        cur = data[j][0]
-    else:
-        cur = data[i][0]
-
-    left = float('inf')
-    right = float('inf')
-    on = light[n] - light[j] + light[i - 1]
-
-    if i > 1:
-        left = f(i - 1, j, 0) + (cur - data[i - 1][0]) * on
-
-    if j < n:
-        right = f(i, j + 1, 1) + (data[j + 1][0] - cur) * on
-
-    dp[i][j][flag] = min(left, right)
-    return dp[i][j][flag]
+def solution(m, b):
+    res = []
+    cnt = 0
+    for k in m:
+        res.append(getRes(cnt,k+cnt, b))
+        cnt += k
+    return res
 
 
-print(f(m, m, 0))
+print(solution([2,5],[3,2,1,9, 1, 3, 4 ]))
+
+
+print(9&1&3&4)
