00aaa4918d0659168749f26d8da6dd9aa62e43a7
syoung7388 <79610047+syoung7388@users.noreply.github.com>
2021-09-01 09:30:05 +0900

    09_01

--- test.py
+++ test.py
@@ -1,37 +1,35 @@
+#20개 - 1000m
+#이동 가능 happy 불가 "sad"
 
+#1. 좌표사이에서 이동가능 한지 안한지 체크
+#2. 이동가능 한지 확인 0~n+1까지
 
+import sys
+sys.setrecursionlimit(10**6)
 
+def check(pos):
+    ch = [[] for _ in range(M+2)]
+    for i in range(M+2):
+        for j in range(M+2):
+            if i == j: continue
+            dis = abs(pos[i][0]-pos[j][0])+abs(pos[i][1]-pos[j][1])
+            if dis <= 1000:
+                ch[i].append(j)
+    return ch
 
-def Back(x, y):
-    if x == 0 or y ==0 :return
-    global res
-    if s1[x-1] == s2[y-1]:
-        res = s1[x-1]+res
-        Back(x-1, y-1)
+def DFS(x):
+    vis[x] = 1
+    for next in ch[x]:
+        if vis[next] == 0:
+            DFS(next)
+    return
+for _ in range(int(input())):
+    M = int(input())
+    pos = [list(map(int, input().split())) for _ in range(M+2)]
+    ch = check(pos)
+    vis = [0]*(M+2)
+    DFS(0)
+    if vis[M+1] == 0:
+        print("sad")
     else:
-        if dp[x-1][y]>= dp[x][y-1]:
-            Back(x-1, y)
-        else:
-            Back(x, y-1)
-            
-
-s1 = list(map(str, input()))
-s2 = list(map(str, input()))
-n1, n2 = len(s1), len(s2)
-dp = [[0]*(n2+1) for _ in range(n1+1)]
-
-for i in range(1, n1+1):
-    for j in range(1, n2+1):
-        if s1[i-1] == s2[j-1]:
-            dp[i][j] = dp[i-1][j-1]+1
-        else:
-            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-
-
-print(dp[-1][-1])
-
-
-res = ""
-Back(n1, n2)
-print(res)
-
+        print("happy")
