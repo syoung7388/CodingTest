42ab6674b8419e76f6d6a771bb9c3c0de39b41ab
syoung7388 <79610047+syoung7388@users.noreply.github.com>
2021-09-08 19:04:15 +0900

    09_08

--- BFS_블록이동하기.py
+++ BFS_블록이동하기.py
@@ -1,43 +1,102 @@
 from collections import deque
 
 
-def solution(gra):
-    N = len(gra)
-    dis = [[[-1]*2 for _ in range(N)] for _ in range(N)]
-    Q = deque()
-    Q.append((0, 1, 0))
-    dis[0][1][0] = 0
-    dx, dy = [-1, 0, 1, 0], [0, 1, 0, -1]
-    while Q:
-        x, y, d = Q.popleft()
-        if x == N-1 and y == N-1:
-            return dis[x][y][d]
+
+
+dx, dy = [-1, 0, 1, 0], [0, 1, 0, -1]
+ch = set()
+
+def Move(T1, T2, gra):
     
-        # 방향 그대로
-        for k in range(4):
-            xx, yy = x+dx[k], y+dy[k]
-            if xx >= N or yy >=N or gra[xx][yy] == 1: continue
-            if dis[xx][yy][d] == -1:
-                Q.append((xx, yy, d))
-                dis[xx][yy][d] = dis[x][y][d] + 1
+    temp = []
+    
+    #위, 아래, 왼, 오 이동
+    for k in range(4):
+        x1, y1, x2, y2 = T1[0]+dx[k], T1[1]+dy[k], T2[0]+dx[k], T2[1]+dy[k] 
+        if gra[x1][y1] == 1 or gra[x2][y2] == 1: continue
+        if ((x1, y1), (x2, y2)) not in ch:
+            temp.append(((x1, y1), (x2, y2)))
+            ch.add(((x1, y1), (x2, y2)))
+    
+    
+    #회전
+    
+    #가로
+    if T1[0] == T2[0]:
         
-        #방향전환  가로:0   세로:1
+        #d = -1 일때
+        if gra[T1[0]-1][T1[1]] == 0 and gra[T2[0]-1][T2[1]] == 0: 
+            
+            minus =  (T1, (T1[0]-1, T1[1]))
+            if minus not in ch:
+                temp.append(minus)
+                ch.add(minus)
+            minus =  (T2, (T2[0]-1, T2[1]))
+            if minus not in ch:
+                temp.append(minus)
+                ch.add(minus)
+           
+        #d = +1 일때
+        elif gra[T1[0]+1][T1[1]] == 0 and gra[T2[0]+1][T2[1]] == 0:
+            
+            plus =  (T1, (T1[0]+1, T1[1]))
+            if  plus not in ch:
+                temp.append(plus)
+                ch.add(plus)
+                
+            plus = (T2, (T2[0]+1, T2[1]))
+            if plus not in ch:
+                temp.append(plus)
+                ch.add(plus)    
         
-        if d == 0: #가로 -> 세로
-            if 0<=x-1 and 0 <= y-1 and gra[x-1][y-1] == 0 and gra[x-1][y] ==0 and dis[x-1][y-1][1] == -1:
-                dis[x-1][y-1][1] = dis[x][y][d] + 1
-                Q.append((x-1, y-1, 1))
-            if x+1 < N and 0 <= y-1 and gra[x+1][y-1] == 0 and gra[x+1][y]==0 and dis[x+1][y-1][1] == -1:
-                dis[x+1][y-1][1] = dis[x][y][d] + 1
-                Q.append((x+1, y-1, 1))
+    
+    #세로
+    else:
+        if gra[T1[0]][T1[1]-1] == 0 and gra[T2[0]][T2[1]-1] == 0:
+            minus = ((T1[0], T1[1]-1), T1)
+            if minus not in ch:
+                temp.append(minus)
+                ch.add(minus)
                 
-        elif d == 1: #세로 -> 가로
-            if 0 <= x-1 and 0 <= y-1 and gra[x-1][y-1] == 0 and gra[x][y-1] == 0 and dis[x-1][y-1][0] == -1:
-                dis[x-1][y-1][0] = dis[x][y][d] + 1
-                Q.append((x-1, y-1, 0))
+            minus =  ((T2[0], T2[1]-1), T2)
+            if minus not in ch:
+                temp.append(minus)
+                ch.add(minus)
                 
-            if 0 <= x-1 and y+1 < N and gra[x-1][y+1] == 0 and gra[x][y+1] == 0 and dis[x-1][y+1][0] == -1:
-                dis[x-1][y+1][0] = dis[x][y][d] + 1
-                Q.append((x-1, y+1, 0))
-    for d in dis:
-        print(d)
+        elif gra[T1[0]][T1[1]+1] == 0 and gra[T2[0]][T2[1]+1] == 0:
+            
+            plus =  ((T1[0], T1[1]+1),T1)
+            if plus not in ch:
+                temp.append(plus)
+                ch.add(plus)
+                
+            plus =  ((T2[0], T2[1]+1),T2)
+            if plus not in ch:
+                temp.append(plus)
+                ch.add(plus)
+    return temp
+        
+    
+
+
+
+def solution(board):
+        N = len(board)
+        ch.add(((1, 1), (1, 2)))
+        Q = deque([((1, 1), (1, 2),0)])
+        
+        gra = [[1 for _ in range(N+2)]]    
+        for b in board:
+            b = [1]+b+[1]
+            gra.append(b)
+        gra.append([1]*(N+2))
+        
+        
+        while Q:
+            T1, T2, cnt = Q.popleft()
+            if T1 == (N, N) or T2 == (N, N):
+                return cnt
+    
+            nQ = Move(T1, T2, gra)
+            for q in nQ:
+                Q.append((q[0], q[1], cnt+1))
--- test.py
+++ test.py
@@ -1,59 +1,37 @@
-from collections import deque
+def seconds(s):
+    h, m, s = map(int, s.split(":"))
+    return h*3600+m*60+s
+def hms(s):
+    h, m, s = s//3600 , (s%3600)//60, (s%3600)%60
+    return '{:02d}:{:02d}:{:02d}'.format(h, m, s)
 
-
-def solution(bro):
-    N = len(bro)
-    gra=  [[-1]*(N+2) for _ in range(N+2)]
-    for i in range(N):
-        for j in range(N):
-            gra[i+1][j+1] = bro[i][j]
-    ch = set([((1, 1), (1, 2))])
+def solution(play, adv, logs):
+    play = seconds(play)
+    adv = seconds(adv)
+    if play <= adv:
+        return "00:00:00"
     
-    Q = deque([([1, 1], [1, 2], 0)])
-    dx, dy =[-1, 0, 1, 0], [0, 1, 0, -1]
+    dp = [0]*(360001)
     
-    while Q:
-        pos1, pos2, cnt = Q.popleft()
-        if pos1 == (N, N) or pos2 == (N,N):
-            return cnt
-        
-        #평행 이동
-        for k in range(4):
-            x1, y1, x2, y2 = pos1[0]+dx[k], pos1[1]+dy[k], pos2[0]+dx[k], pos2[1]+dy[k]
-            npos1 = (x1, y1)
-            npos2 = (x2, y2)
-            if gra[x1][y1]==1 or gra[x2][y2] == 1: continue
-            if (npos1, npos2) not in ch:
-                Q.append((npos1,npos2, cnt+1))
-                ch.add((npos1, npos2))
-        
-        #회전
-        if pos1[0] == pos2[0]: #가로방향
-            for d in [-1, 1]:
-                if gra[pos1[0]+d][pos1[1]] == 1 or gra[pos2[0]+d][pos2[1]] == 1: continue #사각형이 all 0
-                
-                if (npos1, (npos1[0]+d, npos1[1])) not in ch:
-                    Q.append((npos1, (npos1[0]+d, npos1[1]), cnt+1))
-                    ch.add((npos1, (npos1[0]+d, npos1[1])))
-                    
-                if (npos2, (npos2[0]+d, npos2[1])) not in ch:
-                    Q.append((npos2, (npos2[0], npos2[1]), cnt+1))
-                    ch.add((npos2, (npos2[0]+d, npos2[1])))
-        else:
-            for d in [-1, 1]:     
-                
-                if gra[pos1[0]][pos1[1]+d] == 1 or gra[pos2[0]][pos2[1]+d] == 1: continue #사각형이 all 0
-                
-                
-                if ((npos1[0], npos1[1]+d), npos1) not in ch:
-                    Q.append(((npos1[0], npos1[1]+d), npos1, cnt+1))
-                    ch.add(((npos1[0], npos1[1]+d), npos1))
-                if ((npos2[0], npos2[1]+d), npos2) not in ch:
-                    Q.append(((npos2[0], npos2[1]+d), npos2, cnt+1))
-                    ch.add(((npos2[0], npos2[1]+d), npos2))
-                    
-                    
-                
- 
+    for l in logs:
+        st, ed= l.split("-")
+        st = seconds(st)
+        ed = seconds(ed)
+        dp[st] += 1
+        dp[ed] -= -1
+    
+    for i in range(1, 360001):
+        dp[i] += dp[i-1]
 
-print(solution([[0, 0, 0, 1, 1],[0, 0, 0, 1, 0],[0, 1, 0, 1, 1],[1, 1, 0, 0, 1],[0, 0, 0, 0, 0]]))
+    res= 0
+    S = sum(dp[:adv]) #이전 합계
+    maxad = S
+    
+    for j in range(1, 360001-adv):
+        S = S - dp[j-1] + dp[j+adv-1]
+        if S > maxad:
+            res = j
+            maxad = S
+    print(j)
+    return hms(res)
+        
