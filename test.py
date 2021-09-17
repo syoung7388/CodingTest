9ede06d31571e19c2b09bb2a808706e6fe753a8d
syoung7388 <79610047+syoung7388@users.noreply.github.com>
2021-09-17 09:03:02 +0900

    09_17

--- test.py
+++ test.py
@@ -1,7 +1,16 @@
-N,K = map(int, input().split())
-res = 0
-while bin(N).count('1') > K:
-    p = 2**bin(N)[::-1].index('1')
-    res += p
-    N += p
-print(res)
+import math
+
+for _ in range(int(input())):
+    x, y = map(int, input().split())
+    dis = y-x #3
+    
+    n =int(math.sqrt(dis))
+    
+    if dis <= 3:
+        print(dis)
+    elif dis  == n**2:
+        print(n*n-1)
+    elif n**2 < dis <= n**2+n:
+        print(n*n)
+    else:
+        print(n*n+1)
