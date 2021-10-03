fa41a9e61f72fa8ad7a01625a5ea1570c2853a5b
syoung7388 <79610047+syoung7388@users.noreply.github.com>
2021-10-04 06:47:18 +0900

    10_04

--- test.py
+++ test.py
@@ -1,37 +1,27 @@
-from collections import deque
-import math
-dx, dy = [-1, 0, 1, 0], [0, 1, 0, -1]
-def BFS(x, y):
-    Q = deque([(x, y)])
-    dis[x][y] = 0
-    while Q:
-        x, y = Q.popleft()
-        for k in range(4):
-            xx, yy = dx[k]+x, dy[k]+y
-            while True:
-                if not (0<=xx<H and 0<=yy<W): break
-                if gra[xx][yy] == '*': break
-                if dis[x][y]+1 > dis[xx][yy]: break
+N = int(input())
+arr = [list(map(int, input().split())) for _ in range(N)]
+arr.sort(reverse = True)
 
-                Q.append((xx, yy))
-                dis[xx][yy] = dis[x][y]+1
-                xx, yy = xx+dx[k], yy+dy[k]
-    
+res = 0
+for i in range(N):
+    h, x1, x2 = arr[i]
+    left = True
+    right = True
+    for j in range(i+1, N):
+        if h == arr[j][0]: continue
 
+        
+        if left and arr[j][1]<= x1 < arr[j][2]:
 
-W, H = map(int, input().split())
+            left = False
+            res += h-arr[j][0]
+        
+        if right and arr[j][1] <= x2-1 <= arr[j][2]:
 
-gra = [list(map(str, input())) for _ in range(H)]
-
-C = []
-for i in range(H):
-    for j in range(W):
-        if gra[i][j] == 'C':
-            C.append((i, j))
-
-(sx, sy), (ex, ey) = C
-Max = math.inf
-dis = [[Max]*W for _ in range(H)]
-BFS(sx, sy)
-
-print(dis[ex][ey] -1)
+            right = False
+            res += h-arr[j][0]
+    if left:
+        res += h
+    if right:
+        res += h
+print(res)
