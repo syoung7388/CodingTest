60d1b595e492bd5f911dd047a16c65e9477897f9
syoung7388 <79610047+syoung7388@users.noreply.github.com>
2021-10-18 07:31:17 +0900

    10_18

--- test.py
+++ test.py
@@ -1,32 +1,26 @@
-import math
+dic = {2:1, 3:7, 4:4, 5:2, 6:6, 7:8}
 
-
-def GCD(a, b):
-    if a%b == 0:
-        return b
-    return GCD(b, a%b)
-
-
-
-ex, ey = map(int, input().split())
-sx, sy, dx, dy = map(int, input().split())
-if dx == 0:
-    dy = 1
-elif dy == 0:
-    dx = 1
-else:
-    su = int(GCD(dx, dy))
-    dx, dy = dx//su, dy//su
-
-dis = 2147000000
-res = ()
-while True:
-    d = math.sqrt((ex-sx)**2+(ey-sy)**2)
-    if d < dis:
-        res = (sx, sy)
-        dis = d
+for _ in range(int(input())):
+    N = int(input())
+    
+    if N <= 7:
+        small = dic[N]
+    elif N%7 == 1:
+        small = '10'+'8'*(N//7-1)
+    elif N%7 == 3:
+        if N == 10:
+            small = 22
+        else:
+            small = '200' + '8'*(N//7-2)
+    elif N%7 == 4:
+        small = '20'+'8'*(N//7-1)
     else:
-        break
-    sx += dx
-    sy += dy
-print(res[0], res[1])
+        a = N%7 if N%7 else 7
+        b = N//7 if N%7 else N//7-1
+        small = str(dic[a]) + '8'*(b)
+    cnt = N//2
+    if N%2 == 0:
+        large = '1'*(cnt)
+    else:
+        large = '7'+'1'*(cnt-1)
+    print(small, large)
