4d12428cd5dd18eb056f1579fd7a81823c0eea1a
syoung7388 <79610047+syoung7388@users.noreply.github.com>
2021-10-29 07:43:31 +0900

    10_29

--- test.py
+++ test.py
@@ -1,51 +1,29 @@
+def DFS(n):
+    global f
+    ch[n] = 1
+    res[f].append(n)
+    temp = not(f)
+    f = temp
+    for g in G[n]:
+        if ch[g] == 1: continue
+        DFS(g)
+        f = temp
+N= int(input())
 
-
-import sys
-input = sys.stdin.readline
-sys.setrecursionlimit(10000)
-"""
-
-def DFS(L, s):
-    global res
-    if L == 2:
-        S = arr[P[1]] - arr[P[0]-1]
-        print(P, S)
-        if S == K:
-            res += 1
+G= [[] for _ in range(N+1)]
+for  i in range(1, N+1):
+    temp= list(map(int, input().split()))
+    for j in range(temp[0]):
+        G[i].append(temp[j+1])
         
-        return 
-    for i in range(s, N):
-        P[L] = i
-        DFS(L+1, i)
+ch= [0]*(N+1)
+res= [[] for _ in range(2)]
+f = False
+for a in range(1, N+1):
+    if ch[a]==1: continue
+    DFS(a)
 
-N, K = map(int, input().split())
-arr = list(map(int, input().split()))
-
-for i in range(1, N):
-    arr[i] = arr[i-1] + arr[i]
-
-P = [0]*2
-res= 0
-DFS(0, 1)
-print(res+arr.count(K))
-
-
-"""
-import sys
-from itertools import combinations
-input = sys.stdin.readline
-
-
-N, K = map(int, input().split())
-arr = list(map(int, input().split()))
-res = arr.count(K)
-for i in range(1, N):
-    arr[i] = arr[i-1] + arr[i]
-
-for com in combinations(range(1, N), 2):
-    s = arr[com[1]] - arr[com[0]-1]
-    if s == K:
-        res += 1
-        
-print(res + arr.count(K))
-
+print(len(res[0]))
+print(' '.join(sorted(map(str, res[0]))))
+print(len(res[1]))
+print(' '.join(sorted(map(str, res[1]))))  
