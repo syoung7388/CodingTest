2087ee9f07bab86a27c67a22e50340adf8aa8f77
syoung7388 <79610047+syoung7388@users.noreply.github.com>
2021-11-09 07:35:03 +0900

    11_09

--- dp_ABC.py
+++ dp_ABC.py
@@ -1,18 +1,46 @@
+import sys
+def ABC(cnt, a, b, s):
+    if len(s) == N:
+        if cnt == K:
+            print(s)
+            sys.exit(0)
+        return
+    if dp[cnt][a][b] != 0: return 
+
+    dp[cnt][a][b] = 1
+    
+    ABC(cnt, a+1, b, s+'A')
+    ABC(cnt+a, a, b+1, s+'B')
+    ABC(cnt+a+b, a, b, s+'C')
+    
+    
+       
+
+
+N, K = map(int, input().split())
+dp = [[[0]*N for _ in range(N)] for _ in range(N*(N-1)//2)]#2개씩 뽑을 확률
+print(N*(N-1)//2)
+ABC(0, 0, 0, '')
+print(-1)
+
+
+
+"""
 from collections import deque
 import sys
-
 N, K = map(int, input().split())
 
 a= (N-1)//3+1
 b = ((N-1)//3)+1 if N%3 != 1 else (N-1)//3
 c = N - a - b
 
+
     
 temp = (N//3-1)*2+1 if N%3 != 2 else (N//3-1)*2+2
+
 arr = list('C'*c+'B'*b+'A'*a)
+
 num = 0
-
-
 for _ in range(K//(temp+1)):
     arr.insert(0, arr.pop())
     num += (temp+1)
@@ -37,3 +65,6 @@ if num != K:
     print(-1)
 else:
     print(''.join(arr))
+
+
+"""
