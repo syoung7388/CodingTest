dbc5fbe9d4bd66758c34f928b60d7a51f7d72c9f
syoung7388 <79610047+syoung7388@users.noreply.github.com>
2021-12-16 10:03:28 +0900

    10_16

--- 시뮬레이션_어항 정리.py
+++ 시뮬레이션_어항 정리.py
@@ -1,35 +1,49 @@
-from collections import deque
 from collections import defaultdict
 import sys
 input = sys.stdin.readline
 
-def control(gra):
-    temp = defaultdict(int)
-    n, m = len(gra), len(gra[0])
+def roll(gra):
+    order = [0]
     
+    while True:
+        a = order[-1]       
+        n = len(gra[order[0]]) #몇명에게 쌓이는지
+        m = len(order) #쌓이는 칸수 (idx)
+        if (a + n) >= N: break
+        n_order = []
+        for i in range(n): 
+            for j in range(m-1, -1, -1):
+                gra[a+i+1].append(gra[order[j]].pop(0))
+            n_order.append(a+i+1)
+        order = n_order
+
+
+    l = len(gra[order[0]])
+    gra = [gra[i]+[-1]*(l-len(gra[i])) for i in range(order[0], N)]
+
+    return control(gra)
+
+def control(gra):
+    n, m  = len(gra), len(gra[0])
+    fish_mv = defaultdict(int)
     for x in range(n):
         for y in range(m):
             if gra[x][y] == -1: continue
-            for a, b in [(1, 0), (0, 1)]:
-                xx, yy = a + x, y+b
+            for a, b in [(0, 1), (1, 0)]:
+                xx, yy = x+a, y+b
                 if not (0<=xx<n and 0<=yy<m) or gra[xx][yy] == -1: continue
-                diff = abs(gra[xx][yy]-gra[x][y])//5
+                diff = abs(gra[xx][yy] - gra[x][y]) // 5
+
                 if diff == 0: continue
-                a, b = diff, diff
-                if gra[xx][yy] > gra[x][y]:
-                    b = -b
-                else:
-                    a= -a
+                c, d = diff, diff
+                if gra[xx][yy] > gra[x][y]: d = -d
+                else: c = -c
 
-
-                temp[(xx, yy)] += b
-                temp[(x, y)] += a
-
-
-    for k, v in temp.items():
+                fish_mv[(xx, yy)] += d
+                fish_mv[(x, y)] += c
+    
+    for k, v in fish_mv.items():
         gra[k[0]][k[1]] += v
-
-
 
     n_arr = []
     for i in range(n):
@@ -37,62 +51,44 @@ def control(gra):
             if gra[i][j] == -1: break
             n_arr.append(gra[i][j])
     return n_arr
+                
+                
         
-    
-    
-def roll(gra):
-    order = [0]
-    while True:
-        n_order = []
-        a = order[-1] + 1
-        n = len(gra[order[0]])
-        m = len(order)
-        if a + n > N: break #삐쳐나가면 break
-        for i in range(n):
-            for j in range(m-1, -1, -1):
-                gra[a+i].append(gra[order[j]].popleft())
-            n_order.append(a+i)
-        order = n_order
-    l = len(gra[order[0]])   
-    return control([list(gra[i])+[-1]*(l-len(gra[i])) for i in range(order[0], N)])
-
-            
-def turn():
-
+def turn(arr):
     n = N//4
-    gra =[]
+    one = arr[:n]
+    two = arr[n:2*n]
+    three = arr[2*n:3*n]
+    four = arr[3*n:]
+    one.reverse()
+    three.reverse()
 
 
-    for i in range(0, N, n):
-        gra.append(arr[i:i+n])
-
-    gra[2].reverse()
-    gra[0].reverse()
-    gra = [gra[2], gra[1], gra[0], gra[3]]
+    z = list(map(list, zip(four, one, two, three)))
+    return control(z)
     
-    gra = list(map(list, zip(*gra)))
-    for i in range(len(gra)):
-        gra[i].reverse()
     
-    return control(gra)
-
-
+    
+#main
 N, K = map(int, input().split())
 arr = list(map(int, input().split()))
 cnt = 0
 
-        
-while True:
 
+while True:
+    #1)
     mini = min(arr)
     for i in range(N):
-        if mini == arr[i]:
+        if arr[i] == mini:
             arr[i] += 1
-
-
-    gra = [deque([a]) for a in arr]
+   #2) 공중부양, 정리, 일렬
+    gra = [[a] for a in arr]
     arr = roll(gra)
-    arr = turn()
+
+    #3) N//4 turn
+    arr = turn(arr)
+
     cnt += 1
-    if max(arr) - min(arr) <= K:break
+    if max(arr) - min(arr) <= K: break
 print(cnt)
+
