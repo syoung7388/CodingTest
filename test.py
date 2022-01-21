6f33b75ed0a1153047ca22b819dab8da04baac03
syoung7388 <79610047+syoung7388@users.noreply.github.com>
2022-01-21 20:43:58 +0900

    2022_02_21

--- test.py
+++ test.py
@@ -1,81 +1,34 @@
-import sys
-input = sys.stdin.readline
-dx, dy = [-1, -1, 0, 1, 1, 1, 0, -1], [0, -1, -1, -1, 0, 1, 1, 1]
+import numpy as np
 
 
+def init_net():
+    net = {}
+    net['W1'] = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+    net['b1'] = np.array([0.1, 0.2, 0.3])
+    
+    net['W2'] = np.array([[0.1, 0.4], [0.2,0.5], [0.3, 0.6]])
+    net['b2'] = np.array([0.1, 0.2])
 
-def DFS(sx, sy, s, G, F):
-    global res
-    res = max(res, s)
-    gra = [g[:] for g  in G]
-    fish = [f[:] for f in F]
-
-    for i in range(1, 17):
-        if not fish[i]: continue
-        x, y = fish[i]
-        n, d = gra[x][y]
-        if n == -1: continue
-        for j in range(8):
-            k = (d+j)%8
-
-            xx, yy = dx[k]+x, dy[k]+y
-            if not (0<=xx<N and 0<=yy<N) or gra[xx][yy][0] == -1: continue
-
-            #물고기 변경
-            gra[xx][yy], gra[x][y] = [gra[x][y][0], k], gra[xx][yy]
-            fish[i] = [xx, yy]
-            fish[gra[x][y][0]] = [x, y]
-
-            break
+    net['W3'] = np.array([[0.1, 0.3], [0.2,0.4]])
+    net['b3'] = np.array([0.1, 0.2])
 
 
-  
-
-    d = gra[sx][sy][1]
-    gra[sx][sy] = [0, 0] #물고기 먹음
-    for i in range(1, 5):
-        xx, yy = i*dx[d]+sx, i*dy[d]+sy
-        if not (0<=xx<N and 0<=yy<N) or gra[xx][yy][0] == 0: continue
-
-        bf = gra[xx][yy][0]
-        gra[xx][yy][0] = -1
-        fish[bf] = []
-
-        DFS(xx, yy, s+bf ,gra, fish)
-
-        gra[xx][yy][0] = bf
-        fish[bf] = [xx, yy]
-
-        
-        
+    return net
 
 
 
 
+def forward(net, x):
+    w1, w2, w3 = net['W1'], net['W2'], net['W3']                                      
+    b1, b2, b3 = net['b1'], net['b2'], net['b3']
+
+    a1 = np.dot(x, w1) 
+
+    print(a1)
 
 
 
-
-
-
-N = 4
-gra = []
-fish = [[] for _ in range(17)]
-
-for j in range(N):
-    arr = list(map(int, input().split()))
-    a = []
-    for i in range(0, N*2, 2):
-        n, d = arr[i], arr[i+1]
-        a.append([n, d-1])
-        fish[n] = [j, i//2]
-    gra.append(a)
-
-st = gra[0][0][0]
-gra[0][0][0] = -1
-fish[st] = []
-res = 0
-DFS(0, 0, st, gra, fish)
-
-
-print(res)
+x = np.array([1.0, 0.5])
+y = forward(init_net(), x)
+print(y)
+                                        
